2321d889ea27410b2335f1d7b7aa6199e327969d
Amelorate <ameilorate2@gmail.com>
2018-06-12 15:42:42 -0700

    Remove a few unneeded spaces in test.rs

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,4 @@
 use super::*;use std::string::ToString;
-#[test]fn cst_to_string(){let v:Vec<C>=vec![C::C('g'),C::N(103)];let c:CSt=v.into();let s=c.to_string();assert_eq!(&s, "gg");}
-#[test]#[should_panic(expected = "4000000 not valid unicode codepoint: CharTryFromError(())")]
-fn cst_to_string_invalid() {let v:Vec<C>=vec![C::N(4000000)];let c:CSt=v.into();let_s=c.to_string();}
+#[test]fn cst_to_string(){let v:Vec<C>=vec![C::C('g'),C::N(103)];let c:CSt=v.into();let s=c.to_string();assert_eq!(&s,"gg");}
+#[test]#[should_panic(expected="4000000 not valid unicode codepoint: CharTryFromError(())")]
+fn cst_to_string_invalid(){let v:Vec<C>=vec![C::N(4000000)];let c:CSt=v.into();let_s=c.to_string();}
